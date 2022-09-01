90068eae7fd9d7bf23f90c821a78eaa609f1d667
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-09-01 19:21:55 +0000

    Components and props

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FunctionaGreeting = () => <h1>Hello from React!</h1>;
+
+// function FunctionaGreeting() {
+//     return <h1>Hello From react</h1>
+// }
+
+export default FunctionaGreeting;
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const FunctionaGreetingWithProps = () => <h1>Hello</h1>;
+
+export default FunctionaGreetingWithProps;
