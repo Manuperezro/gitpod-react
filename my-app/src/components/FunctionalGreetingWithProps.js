56316c24a23867f5d3331193ed684f9c5386a927
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-09-01 19:30:33 +0000

    Components and props 2

--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -1,5 +1,8 @@
 import React from "react";
 
-const FunctionaGreetingWithProps = () => <h1>Hello</h1>;
+const FunctionaGreetingWithProps = (props) => {
+    console.log(props);
+    return <h1>Hello!, {props.greeting}</h1>;
+}
 
 export default FunctionaGreetingWithProps;
